9586d0db53ccb1ed10e216fce0b64aee84a953ec
Lieselot Geirnaert <lieselot.geirnaert@gmail.com>
2019-10-23 16:19:06 +0200

    fix randomizer bug

--- src/index.js
+++ src/index.js
@@ -1,38 +1,42 @@
 import "./style.css";
 {
-
+  let allQuotesObject;
 
   const loadJSON = async () => {
     const url = `./src/assets/data/quotes.json`;
-
     const response = await fetch(url);
     const quotesObject = await response.json();
-    getRandomQuote(quotesObject);
+    allQuotesObject = quotesObject;
   }
 
-  const getRandomQuote = quotesObject => {
-    console.log(quotesObject.quotes.length);
-
-    let randomNumber = Math.floor((Math.random() * quotesObject.quotes.length));
-    let randomQuote = quotesObject.quotes[randomNumber].quote;
+  const getRandomQuote = () => {
+    let randomNumber = Math.floor((Math.random() * allQuotesObject.quotes.length));
+    let randomQuote = allQuotesObject.quotes[randomNumber].quote;
     console.log(randomQuote);
+
+    const $quote = document.querySelector('.quoterandom');
+    console.log($quote);
+    $quote.innerHTML = randomQuote;
   };
 
-  const getAllQuotes = quotesObject => {
-    quotes.forEach(quote => {
-      const $li = document.createElement(`li`);
-      $li.classList.add(`xxx`);
+  // const getAllQuotes = quotesObject => {
+  //   quotes.forEach(quote => {
+  //     const $li = document.createElement(`li`);
+  //     $li.classList.add(`xxx`);
 
-      $li.innerHTML = `  `;
-      $list.appendChild($li);
-    })
-  }
+  //     $li.innerHTML = `  `;
+  //     $list.appendChild($li);
+  //   })
+  // }
 
   const init = () => {
     console.log(`test`);
     loadJSON();
 
-    getAllQuotes();
+    const $randomButton = document.querySelector('.quotebutton__random');
+    $randomButton.addEventListener('click', getRandomQuote);
+
+    // getAllQuotes();
   };
 
   init();
